05a1d1993a94ef843d34ae70ac94dec6f62ebc0d
realicraft <39655846+realicraft@users.noreply.github.com>
2023-11-07 21:47:09 -0800

    Import everything
    thanks gilbert ¦:|

---
+++ Metro2/languages/Settings.english.php
@@ -0,0 +1,13 @@
+<?php
+// Version: 2.1.0; Settings
+
+global $settings;
+
+// argument(s): images_url as saved in settings
+$txt['theme_thumbnail_href'] = '%1$s/thumbnail.png';
+$txt['theme_description'] = 'Flat, minimalist, and striking in color, this style has a sleek, quick, modern, and refresh look.';
+
+$txt['variant_red'] = 'Red';
+$txt['variant_green'] = 'Green';
+$txt['variant_blue'] = 'Blue';
+?>
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TBGs-SMF-Styles but better
- A repository containing various TBG styles for SMF.
+A repository containing various TBG styles for SMF.
+Based off of tbgers/TBGs-SMF-Styles ([https://github.com/tbgers/TBGs-SMF-Styles]).
